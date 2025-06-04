c67e215f716f6d361205d68d668464c948c2a886
Sebastián-Markoja <sebaacc11@gmail.com>
2025-06-04 13:45:10 -0300

    modificando auditoria e implementando nueva clase servicio auditoria

---
+++ pryMarkoja_IEFI/pryMarkoja_IEFI/Clases/clsAuditoriaService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryMarkoja_IEFI.Clases
+{
+    public class clsAuditoriaService
+    {
+        public DataTable ObtenerTiemposPorNombre(string filtroNombre)
+        {
+            return EjecutarConsulta(filtroNombre, false, null, null);
+        }
+
+        public DataTable ObtenerTiemposPorFechas(DateTime desde, DateTime hasta)
+        {
+            return EjecutarConsulta("", true, desde, hasta);
+        }
+
+        public DataTable ObtenerTiemposPorNombreYFechas(string filtroNombre, DateTime desde, DateTime hasta)
+        {
+            return EjecutarConsulta(filtroNombre, true, desde, hasta);
+        }
+
+        private DataTable EjecutarConsulta(string filtroNombre, bool usarFechas, DateTime? desde, DateTime? hasta)
+        {
+            DataTable tabla = new DataTable();
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(clsConexionBD.CadenaConexion))
+                {
+                    conexion.Open();
+
+                    string query = @"
+                        SELECT 
+                            u.NombreUsuario AS [Nombre de Usuario],
+                            u.Nombre,
+                            u.Apellido,
+                            u.PuestoTrabajo AS [Puesto de Trabajo],
+                            COUNT(s.Id) AS [Cantidad de Sesiones],
+                            SUM(DATEDIFF(SECOND, s.FechaInicio, s.FechaFin)) AS [Total de Segundos Trabajados]
+                        FROM Usuario u
+                        INNER JOIN SesionTrabajo s ON u.Id = s.UsuarioId
+                        WHERE u.NombreUsuario LIKE @filtroNombre
+                        AND s.FechaFin IS NOT NULL";
+
+                    if (usarFechas)
+                    {
+                        query += " AND s.FechaInicio >= @desde AND s.FechaInicio <= @hasta";
+                    }
+
+                    query += @"
+                        GROUP BY u.NombreUsuario, u.Nombre, u.Apellido, u.PuestoTrabajo
+                        ORDER BY u.NombreUsuario";
+
+                    using (SqlCommand comando = new SqlCommand(query, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@filtroNombre", $"%{filtroNombre}%");
+
+                        if (usarFechas)
+                        {
+                            comando.Parameters.AddWithValue("@desde", desde ?? DateTime.MinValue);
+                            comando.Parameters.AddWithValue("@hasta", hasta ?? DateTime.MaxValue);
+                        }
+
+                        using (SqlDataAdapter adaptador = new SqlDataAdapter(comando))
+                        {
+                            adaptador.Fill(tabla);
+
+                            tabla.Columns.Add("Tiempo Trabajado Formateado", typeof(string));
+                            foreach (DataRow fila in tabla.Rows)
+                            {
+                                int totalSegundos = Convert.ToInt32(fila["Total de Segundos Trabajados"]);
+                                TimeSpan tiempo = TimeSpan.FromSeconds(totalSegundos);
+                                fila["Tiempo Trabajado Formateado"] = $"{(int)tiempo.TotalHours:D2}:{tiempo.Minutes:D2}:{tiempo.Seconds:D2}";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener datos de auditoría: " + ex.Message, ex);
+            }
+
+            return tabla;
+        }
+        public DataTable ObtenerTodosLosTiempos()
+        {
+            return EjecutarConsulta("", false, null, null);
+        }
+    }
+}
--- pryMarkoja_IEFI/pryMarkoja_IEFI/frmAuditoria.cs
+++ pryMarkoja_IEFI/pryMarkoja_IEFI/frmAuditoria.cs
@@ -14,97 +14,49 @@ namespace pryMarkoja_IEFI
 {
     public partial class frmAuditoria : Form
     {
+        clsAuditoriaService servicioAuditoria = new clsAuditoriaService();
         public frmAuditoria()
         {
             InitializeComponent();
             formateoInicial();
         }
+        private void CargarSoloPorNombre()
+        {
+            string nombreBuscado = txtBuscar.Text.Trim();
+            var tabla = servicioAuditoria.ObtenerTiemposPorNombre(nombreBuscado);
+            dgvAuditoria.DataSource = tabla;
+            dgvAuditoria.Columns["TotalSegundosTrabajados"].Visible = false;
+        }
 
+        private void CargarSoloPorFechas()
+        {
+            DateTime desde = dtpDesde.Value.Date;
+            DateTime hasta = dtpHasta.Value.Date.AddDays(1).AddSeconds(-1);
+            var tabla = servicioAuditoria.ObtenerTiemposPorFechas(desde, hasta);
+            dgvAuditoria.DataSource = tabla;
+            dgvAuditoria.Columns["TotalSegundosTrabajados"].Visible = false;
+        }
+
+        private void CargarPorNombreYFechas()
+        {
+            string nombreBuscado = txtBuscar.ToString();
+            DateTime desde = dtpDesde.Value.Date;
+            DateTime hasta = dtpHasta.Value.Date.AddDays(1).AddSeconds(-1);
+            var tabla = servicioAuditoria.ObtenerTiemposPorNombreYFechas(nombreBuscado, desde, hasta);
+            dgvAuditoria.DataSource = tabla;
+            dgvAuditoria.Columns["TotalSegundosTrabajados"].Visible = false;
+        }
+        
         private void btnVolver_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private void TiempoSistema_Tick(object sender, EventArgs e)
-        {
-            lblFechaHoraSistema.Text = DateTime.Now.ToString();
-        }
-        private void CargarTiemposDeUsuarios(string filtroNombre = "", bool usarFechas = false, DateTime? desde = null, DateTime? hasta = null)
-        {
-            try
-            {
-                using (SqlConnection conexion = new SqlConnection(clsConexionBD.CadenaConexion))
-                {
-                    conexion.Open();
-
-                    string query = @"
-                    SELECT 
-                        u.NombreUsuario,
-                        u.Nombre,
-                        u.Apellido,
-                        u.PuestoTrabajo,
-                        COUNT(s.Id) AS CantidadSesiones,
-                        SUM(DATEDIFF(SECOND, s.FechaInicio, s.FechaFin)) AS TotalSegundosTrabajados
-                    FROM Usuario u
-                    INNER JOIN SesionTrabajo s ON u.Id = s.UsuarioId
-                    WHERE u.NombreUsuario LIKE @filtroNombre
-                    AND s.FechaFin IS NOT NULL";
-
-                    if (usarFechas)
-                    {
-                        query += " AND s.FechaInicio >= @desde AND s.FechaInicio <= @hasta";
-                    }
-
-                    query += @"
-                    GROUP BY u.NombreUsuario, u.Nombre, u.Apellido, u.PuestoTrabajo
-                    ORDER BY u.NombreUsuario";
-
-                    using (SqlCommand comando = new SqlCommand(query, conexion))
-                    {
-                        comando.Parameters.AddWithValue("@filtroNombre", $"%{filtroNombre}%");
-
-                        if (usarFechas)
-                        {
-                            comando.Parameters.AddWithValue("@desde", desde ?? DateTime.MinValue);
-                            comando.Parameters.AddWithValue("@hasta", hasta ?? DateTime.MaxValue);
-                        }
-
-                        using (SqlDataAdapter adaptador = new SqlDataAdapter(comando))
-                        {
-                            DataTable tabla = new DataTable();
-                            adaptador.Fill(tabla);
-
-                            tabla.Columns.Add("TiempoTrabajadoFormateado", typeof(string));
-                            foreach (DataRow fila in tabla.Rows)
-                            {
-                                int totalSegundos = Convert.ToInt32(fila["TotalSegundosTrabajados"]);
-                                TimeSpan tiempo = TimeSpan.FromSeconds(totalSegundos);
-                                fila["TiempoTrabajadoFormateado"] = $"{(int)tiempo.TotalHours:D2}:{tiempo.Minutes:D2}:{tiempo.Seconds:D2}";
-                            }
-
-                            dgvAuditoria.DataSource = tabla;
-                            if (dgvAuditoria.Columns.Contains("TotalSegundosTrabajados"))
-                            {
-                                dgvAuditoria.Columns["TotalSegundosTrabajados"].Visible = false;
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ocurrió un error. " + ex);
-            }
-        }
-
-
-
         private void btnFiltrar_Click(object sender, EventArgs e)
         {
             if (txtBuscar.Text != "")
             {
-                string filtro = txtBuscar.Text.Trim();
-                CargarTiemposDeUsuarios(filtro, false);
+                CargarSoloPorNombre();
             }
             else
             {
@@ -115,9 +67,7 @@ namespace pryMarkoja_IEFI
         private void btnFiltrarFecha_Click(object sender, EventArgs e)
         {
 
-            DateTime desde = dtpDesde.Value.Date;
-            DateTime hasta = dtpHasta.Value.Date.AddDays(1).AddSeconds(-1); // incluye todo el día
-            CargarTiemposDeUsuarios("", true, desde, hasta);
+            CargarSoloPorFechas();
         }
 
         private void btnMostrarTodos_Click(object sender, EventArgs e)
@@ -127,9 +77,17 @@ namespace pryMarkoja_IEFI
 
         private void formateoInicial()
         {
-            dtpDesde.Value = DateTime.Today.AddDays(-7); // Últimos 7 días
-            dtpHasta.Value = DateTime.Today;
-            CargarTiemposDeUsuarios("", false, dtpDesde.Value, dtpHasta.Value);
+            servicioAuditoria.ObtenerTodosLosTiempos();
+            dgvAuditoria.DataSource = servicioAuditoria.ObtenerTodosLosTiempos();
+        }
+        private void TiempoSistema_Tick(object sender, EventArgs e)
+        {
+            lblFechaHoraSistema.Text = DateTime.Now.ToString();
+        }
+
+        private void btnFiltrarFechaYNombre_Click(object sender, EventArgs e)
+        {
+            CargarPorNombreYFechas();
         }
     }
 }
